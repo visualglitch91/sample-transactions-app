dd1464c69fbacf73cab964be8c43f20a58ec7eff
@ericaeumesma <visualglitch91@gmail.com>
2019-09-27 21:04:13 +0200

    Add a button to remove transactions

--- src/components/transaction-list/transaction.js
+++ src/components/transaction-list/transaction.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import IconButton from '@material-ui/core/IconButton'
+import Icon from '@material-ui/core/Icon'
+import { useDispatch } from '../../store'
 import useCurrency from '../../hooks/use-currency'
 
 function Transaction({ id, description, date, amount, type }) {
   const currency = useCurrency()
+  const dispatch = useDispatch()
+
+  function remove() {
+    dispatch('REMOVE', id)
+  }
 
   return (
-    <ListItem>
+    <ListItem id={`transaction__${id}`}>
       <ListItemText
         primary={description}
         secondary={currency(amount, { negative: type === 'debit' })}
         secondaryTypographyProps={{ style: { color: type === 'debit' ? 'red' : 'green' } }}
       />
+
+      <ListItemSecondaryAction>
+        <IconButton
+          id={`transaction__${id}__remove`}
+          edge="end"
+          aria-label="remover transação"
+          onClick={remove}
+        >
+          <Icon>delete</Icon>
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   )
 }
